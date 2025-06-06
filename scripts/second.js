f5f699a9861889d53488e7e952c8a36aeae9d68e
itttm127 <itttm127@gmail.com>
2025-06-06 04:51:02 -0400

    chore: update second section

--- scripts/second.js
+++ scripts/second.js
@@ -1,45 +1,61 @@
-gsap.registerPlugin(ScrollTrigger, SplitText);
+gsap.registerPlugin(ScrollTrigger, Observer, SplitText);
 
-// Target only the second section
 const second_section = document.querySelector(".second-section");
 const second_outer = second_section.querySelector(".outer");
 const second_inner = second_section.querySelector(".inner");
 const second_heading = second_section.querySelector(".book-quote p");
 const second_bg = second_section.querySelector(".book-cover img");
-const second_price = second_section.querySelector(".book-price ");
+const second_price = second_section.querySelector(".book-price");
 
 const second_splitHeading = new SplitText(second_heading, {
   type: "chars,words,lines",
   linesClass: "clip-text",
 });
 
-// Set initial state: push it down and hide it
+// Initial state
 gsap.set(second_section, { autoAlpha: 0 });
 gsap.set(second_outer, { yPercent: 100 });
 gsap.set(second_inner, { yPercent: -100 });
 
-ScrollTrigger.create({
-  trigger: second_section,
-  start: "top bottom", // when top of section hits bottom of viewport
-  end: "top center", // optional
-  scrub: 2, // enables scroll-synced animation
-  onUpdate: (self) => {
-    if (self.progress > 0 && self.progress < 1) {
-      gsap.to(second_section, { autoAlpha: 1, overwrite: true });
-    }
-  },
-  onEnter: () => second_animateIn(),
-  onEnterBack: () => second_animateIn(),
+let currentDirection = 1;
+
+// Detect scroll direction only
+Observer.create({
+  type: "wheel,touch,pointer",
+  onUp: () => (currentDirection = -1),
+  onDown: () => (currentDirection = 1),
+  tolerance: 10,
+  preventDefault: false,
 });
 
-function second_animateIn() {
+// ScrollTrigger that triggers every time section enters view
+ScrollTrigger.create({
+  trigger: second_section,
+  start: "top 75%",
+  end: "bottom 25%",
+  onEnter: () => second_animateIn(currentDirection),
+  onEnterBack: () => second_animateIn(currentDirection),
+});
+
+function second_animateIn(direction) {
+  const d = direction === -1 ? -1 : 1;
   const tl = gsap.timeline();
 
-  gsap.set(second_section, { autoAlpha: 1 });
-  tl.addLabel("second_section")
+  gsap.fromTo(
+    second_section,
+    { autoAlpha: 0, yPercent: 50 * d },
+    {
+      autoAlpha: 1,
+      yPercent: direction === -1 ? 25 : -25,
+      duration: 2,
+      ease: "power2",
+    }
+  );
+
+  tl.addLabel("start")
     .fromTo(
       second_splitHeading.chars,
-      { autoAlpha: 0, yPercent: 150 },
+      { autoAlpha: 0, yPercent: 150 * d },
       {
         autoAlpha: 1,
         yPercent: 0,
@@ -50,18 +66,18 @@ function second_animateIn() {
           from: "random",
         },
       },
-      "second_section+=0.1"
+      "start+=0.1"
     )
     .fromTo(
       second_bg,
-      { yPercent: 15 },
+      { yPercent: 15 * d },
       { yPercent: 0, duration: 2, ease: "power2" },
-      "second_section+=0.2"
+      "start+=0.2"
     )
     .fromTo(
       second_price,
       { xPercent: 105, rotate: "360deg" },
       { xPercent: 0, rotate: 0, duration: 2 },
-      "second_section+=0.2"
+      "start+=0.2"
     );
 }
